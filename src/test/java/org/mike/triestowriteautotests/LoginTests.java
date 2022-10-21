6e2d7ff7c8a585439d7a8b25db2510ce830d1188
fh279 <fh279@yandex.ru>
2022-10-21 15:11:20 +0300

    checks from p.4 (AndrewG bio) implemented
    Implemented all checks on Andrew Gachechiladze biographe. Point 4 - done

--- src/test/java/org/mike/triestowriteautotests/LoginTests.java
+++ src/test/java/org/mike/triestowriteautotests/LoginTests.java
@@ -95,7 +95,7 @@ public class LoginTests {
     }
 
     @Test
-    public void linksValidation_NewSession_YT_FB_Twitr_LinkdIn(){
+    public void linksValidation_NewSession_YT_FB_Twitr_LinkdIn() {
         /* 3. Validate links on social media(youtube, facebook, twitter, inst)
          * создать драйвер
          * Браузер на полный экран
@@ -130,16 +130,49 @@ public class LoginTests {
     }
 
     @Test
-    public void check_AndrewG_Position(){
-     /*
-     * Создаем драйвер, браузер на полный экран, неявное ожидание 8 секунд
-     * Заходим на https://www.intermedia.com/about-us/who-we-are
-     * Найти область, где размещено фото Андрея Г.
-     *      (xPath тут: "//*[@id="about_overview"]/section[3]/div[1]/div/div[1]/div[5]") ,
-     *      запихать найденную область в WebElement area_AndrewG
-     * Написать проверку что в этой области есть текст "Andrew Gachechiladze EVP of Product Development and Engineering"
-     * */
-        
+    public void check_AndrewG_Position() {
+        /*
+         * Создаем драйвер, браузер на полный экран, неявное ожидание 8 секунд
+         * Заходим на https://www.intermedia.com/about-us/who-we-are
+         * Найти область, где размещено имя Андрея Г.
+         *      (xPath тут: "//*[@id="about_overview"]/section[3]/div[1]/div/div[1]/div[5]") , - блин, это фигня какая-то
+         *       //*[@id="about_overview"]/section[3]/div[1]/div/div[1]/div[5]/a/div/p[1]   -   вот тут имя Андрея.
+         * Заводим ожидаемое Имя в переменную.
+         * Проверяем соответствие ожидаемого и действительного
+         * То же делаем для должности Андрея.
+         * Найдем класс, в котором хранятся параграфы (<p>) с биографией Андрея, положим его в WebElement info_AndrewG_pt1
+         * Создадим строковый массив, куда запишем ожидаемые результаты по описанию биографии Андрея Г. (длинные, да, а что делать...)
+         * Считаем параграфы, где франится биография Андрея и положим её в List<WebElement> paragraphsOfAndrewBio
+         * Сконвертируем вебдрайвер в JSвебдрайвер для того чтобы из параграфа вытащить innerText.
+         * Пройдемся циклом по элементам paragraphsOfAndrewBio и сопоставим их с ожидаемым результатом.
+         * */
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+        driver.get("https://www.intermedia.com/about-us/who-we-are");
+
+        WebElement name_AndrewG = driver.findElement(By.xpath("//*[@id=\"about_overview\"]/section[3]/div[1]/div/div[1]/div[5]/a/div/p[1]"));
+        String expectedAndrewG_name = "Andrew Gachechiladze";
+        Assert.assertEquals(expectedAndrewG_name, name_AndrewG.getText());
+
+        WebElement duty_AndrewG = driver.findElement(By.xpath("//*[@id=\"about_overview\"]/section[3]/div[1]/div/div[1]/div[5]/a/div/p[2]"));
+        String expectedAndrewG_duty = "EVP of Product Development and Engineering";
+        Assert.assertEquals(expectedAndrewG_duty, duty_AndrewG.getText());
+
+        WebElement allBioOfAndrew = driver.findElement(By.xpath("//*[@id=\"leader_Andrew_Gachechiladze\"]/div[2]/div[2]"));
+        String[] expectedResultsAndrewBio =
+                {"Andrew Gachechiladze leads Intermedia’s product development and engineering groups, overseeing all software development for Intermedia’s cloud platform.",
+                        "With the company since 2001, Gachechiladze has set the foundation for Intermedia’s cloud management and partner portal platforms. He’s been directly involved in scaling the broad suite of integrated cloud applications Intermedia offers today, including third-party products such as Microsoft® Office 365® and proprietary offerings like unified communications, SecuriSync all-in-one backup and file sharing, and our security services portfolio. Further strengthening Intermedia’s solution portfolio, Gachechiladze has also played an integral role in two successful acquisitions.",
+                        "Under Gachechiladze’s leadership, the development team has grown from a group of less than 10 individuals to 100+; from a single office to four different development centers using multiple technologies, including Mountain View, CA, Bellevue, WA, St. Petersburg, Russia and Bristol, UK.",
+                        "During his tenure with the company, Gachechiladze has previously been a senior software developer and a product manager for cloud-based Exchange. Prior to Intermedia, Gachechiladze has held technical and software development roles with a range of companies, including ThinkWave, a cloud-based school administration system. Gachechiladze has a Masters degree in Physics and Computer Science from Tbilisi State University."
+                };
+
+        List<WebElement> paragraphsOfAndrewBio = allBioOfAndrew.findElements(By.tagName("p"));
+        JavascriptExecutor jsDriver = (JavascriptExecutor) driver;
+        for (int i = 0; i < paragraphsOfAndrewBio.size(); i++) {
+            String actualText = jsDriver.executeScript("return arguments[0].innerText", paragraphsOfAndrewBio.get(i)).toString();
+            Assert.assertEquals(expectedResultsAndrewBio[i], actualText);
+        }
     }
 /*Задача
 Open:
